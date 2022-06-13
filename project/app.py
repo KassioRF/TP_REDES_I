b9aeb9c2c3919bef9c53b57c661ca9a9d3278b8c
KassioRF <kassio-rf@hotmail.com>
2022-06-12 22:36:41 -0300

    added user lobby

--- project/app.py
+++ project/app.py
@@ -1,42 +1,68 @@
-from flask import Flask, request
-from flask_socketio import SocketIO, emit
+from flask import Flask, request, redirect, url_for
+from flask_socketio import SocketIO, send, emit
 
 from flask import render_template
+import time
 import pprint
+
 
 app = Flask(__name__)
 io = SocketIO(app, cors_allowed_origins="*")
 
 
-clients = []
-
-
-@app.route("/index")
-def index():
-    # return "<p>Hello, World!</p>"
-    return render_template("index.html")
+clients = {}
 
 
 @app.route("/")
-def chat():
-    return render_template("chat.html")
+def index():
+    # return "<p>Hello, World!</p>"
+    print("\n\t - index")
+    print(clients)
+    print("\n\t - --")
+    return render_template("enter.html")
+
+
+@app.route("/<username>")
+def chat(username):
+    if username not in clients.keys():
+        # Garante que só sejam redirecionados para página de msg quem está na lista de clientes
+        return redirect(url_for("index"))
+
+    return render_template("chat.html", clients=clients)
+
+
+# @app.route("/")
+# def chat():
+#     return render_template("enter.html")
 
 
 # @TODO adicionar o nome do cliente na lista
 @io.on("connect")
-def onConnect():
-    uId = request.sid
-    ip = request.remote_addr
-    port = request.environ.get("REMOTE_PORT")
-
-    clients.append({"sId": uId, "ip": f"{ip}:{port}"})
-
-    print(pprint.pformat(clients))
+def connect():
+    print(f"\n ---- conected ----\n")
 
 
-@io.on("sendMessage")
-def send_handler(msg):
-    print(msg)
+@io.on("username", namespace="/enter")
+def enter(username):
+    if username not in clients.keys():
+        clients[username] = request.sid
+        # redirect(url_for("chat", username=username))
+        print(f"\n\t --- EMIT ---\n\t")
+        io.emit(
+            "enter", {"url": url_for("chat", username=username)}, namespace="/enter"
+        )
+
+        # time.sleep(1)
+
+        io.emit("update", {"clients": clients}, namespace="/lobby")
+
+    else:
+        print(f"\n\t --- EMIT FAIL ---\n\t")
+        io.emit(
+            "enter_error", "O Nome já está sendo usado, tente outro", namespace="/enter"
+        )
+
+    print(clients)
 
 
 if __name__ == "__main__":
